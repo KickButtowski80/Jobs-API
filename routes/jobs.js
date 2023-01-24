5cf45c899b48f3f3d27b56742f36c4bba44d59e5
KickButtowski80 <pazpaz22@yahoo.com>
2023-01-23 18:52:36 -0800

    add the create job route

--- routes/jobs.js
+++ routes/jobs.js
@@ -8,7 +8,7 @@ const {
     updateJob,
     deleteJob, } = require('../controllers/jobs')
 
-router.route('/').get(getAllJobs)
+router.route('/').post(createJob).get(getAllJobs)
 router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob)
 
 module.exports = router
