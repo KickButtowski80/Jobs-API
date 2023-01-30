9ffc10982c6630709103a28724e6f1ba659d7f2b
KickButtowski80 <pazpaz22@yahoo.com>
2023-01-30 15:50:57 -0800

    covered mongoose errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,27 @@
-const { CustomAPIError } = require('../errors')
+
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INSUFFICIENT_SPACE_ON_RESOURCE,
+    msg: err.message || '😨Something went wrong. 👍 please try again'
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((e) => e.message)
+      .join(',')
+    customError.statusCode = 400
+  }
+  if (err.code && err.code === 11000) {
+    customError.msg = `😨Duplicate value entered for ${Object.keys(err.keyValue)} field,👍 please choose another value`
+  } customError.statusCode = 404
+  
+  if (err.name === 'CastError') {
+    customError.msg = `🛑No Item Found with id: ${err.value}`
+    customError.statusCode = 400
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
