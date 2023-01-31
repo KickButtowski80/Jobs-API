53d7ffe12e787bca7e2218ef56b786c432e73f6a
KickButtowski80 <pazpaz22@yahoo.com>
2023-01-30 16:49:07 -0800

    setup security packages

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limiter')
+
 const express = require('express');
 const app = express();
+
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  windowsMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+}))
+app.use(helmet());
+app.use(cors())
+app.use(xss())
+
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 // error handler
